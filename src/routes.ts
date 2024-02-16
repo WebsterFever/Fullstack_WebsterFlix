30b94b28b92857563c2d55650313c5393d6237d8
WebsterFever <webstersevenlanguages@gmail.com>
2024-02-16 00:06:27 -0500

    User register

--- src/routes.ts
+++ src/routes.ts
@@ -3,14 +3,19 @@
 import express from 'express'
 import { categoriesController } from './controllers/categoriesController'
 import { coursesController } from './controllers/coursesController'
+import { episodesController } from './controllers/episodesController'
+import { authController } from './controllers/authController'
 
 const router = express.Router()
 
+
+router.post('/auth/register' , authController.register)
 router.get('/categories', categoriesController.index)
 router.get('/categories/:id', categoriesController.show)
 router.get('/courses/featured', coursesController.featured)
 router.get('/courses/newest', coursesController.newest)
 router.get('/courses/search', coursesController.search)
 router.get('/courses/:id', coursesController.show)
+router.get('/episodes/stream' , episodesController.stream)
 
 export { router }
--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,8 @@ import { router } from "./routes"
 const app = express()
 
 app.use(express.static('public'))
+
+app.use(express.json())
 app.use(adminjs.options.rootPath , adminJsRouter)
 
 app.use(router)
